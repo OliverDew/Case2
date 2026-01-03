ed09d19b71c9b54e8d9ec723104adb3b84763617
phuongthao <phuongthao30823@gmail.com>
2026-01-03 23:44:03 +0100

    Test

--- test/project.py
+++ test/project.py
@@ -4,4 +4,8 @@ df = read_csv("case2.csv", sep=";")
 df = df.drop(columns=["commodity", "comm_code"])
 print(df.head())
 print(df.columns)
-df = df[df["country_or_area"] != "EU-28"]
+df = df[~df["country_or_area"].isin([
+    "EU-28",
+    "So. African Customs Union"])]
+df["country_or_area"] = df["country_or_area"].replace(
+    "Fmr Fed. Rep. of Germany", "Germany")
