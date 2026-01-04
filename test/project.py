62adf74ff87235ca9a3693f0771a6e175645a34a
vera0806 <vera.woermann@gmail.com>
2026-01-04 15:59:42 +0100

    deal with missing values

--- test/project.py
+++ test/project.py
@@ -14,3 +14,8 @@ df = df.groupby('country_or_area', group_keys=False).apply(
     lambda x: x.sample(frac=0.1))
 df = df.reset_index(drop=True)
 df.to_csv("case2_sampled.csv", index=False)
+
+df.info()
+print(df.info())
+df['weight_kg'] = df['weight_kg'].fillna(df['weight_kg'].median())
+print(df.info())
